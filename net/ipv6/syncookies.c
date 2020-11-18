b43bd3e2e52984b7b01085685029b1dfc8702998
TogoFire <italomellopereira@gmail.com>
2020-11-18 18:03:08 -0300

    syncookies.c: bump too few arguments to function call, expected 8, have 7
    -log:
    ../net/ipv4/syncookies.c:400:42: error: too few arguments to function call, expected 8, have 7
                                      dst_metric(&rt->dst, RTAX_INITRWND));
    ../net/ipv6/syncookies.c:400:42: error: too few arguments to function call, expected 8, have 7
                                      dst_metric(&rt->dst, RTAX_INITRWND));
    
    Signed-off-by: TogoFire <italomellopereira@gmail.com>

--- net/ipv6/syncookies.c
+++ net/ipv6/syncookies.c
@@ -246,7 +246,7 @@ struct sock *cookie_v6_check(struct sock *sk, struct sk_buff *skb)
 	tcp_select_initial_window(full_space, req->mss,
 				  &req->rsk_rcv_wnd, &req->rsk_window_clamp,
 				  ireq->wscale_ok, &rcv_wscale,
-				  dst_metric(dst, RTAX_INITRWND));
+				  dst_metric(dst, RTAX_INITRWND), sk);
 
 	ireq->rcv_wscale = rcv_wscale;
 	ireq->ecn_ok = cookie_ecn_ok(&tcp_opt, sock_net(sk), dst);
